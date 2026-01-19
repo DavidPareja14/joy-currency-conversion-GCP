232d2beb7d2a2c3af93cdddec4184e5b17afa448
David Pareja Arango <davidpaar14@gmail.com>
2026-01-18 23:10:33 -0500

    chore: add secret to Secret Manager and get it from local

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 )
+
+func getSecret(secretName string) string {
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: secretName, // projects/PROJECT_ID/secrets/API_KEY/versions/latest
+	}
+
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		log.Fatalf("failed to access secret: %v", err)
+	}
+
+	return string(result.Payload.Data)
+}
 
 func main() {
 	// Load environment variables from .env file
@@ -19,7 +43,7 @@ func main() {
 	// Get API key from environment
 	apiKey := os.Getenv("EXCHANGE_RATES_API_KEY")
 	if apiKey == "" {
-		log.Fatal("EXCHANGE_RATES_API_KEY environment variable is required")
+		log.Println("EXCHANGE_RATES_API_KEY environment variable is required")
 	}
 
 	http.HandleFunc("/convert", convertHandler)
@@ -36,6 +60,10 @@ func main() {
 func convertHandler(w http.ResponseWriter, r *http.Request) {
 	apiKey := os.Getenv("EXCHANGE_RATES_API_KEY")
 
+	if apiKey == "" {
+		projectId := os.Getenv("PROJECT_ID")
+		apiKey = getSecret("projects/" + projectId + "/secrets/EXCHANGE_RATES_API_KEY/versions/latest")
+	}
 	// Call the conversion logic
 	result, err := ConvertEURToCOP(apiKey)
 	if err != nil {
